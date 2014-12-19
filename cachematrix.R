9abe7fe6decd7a562a7706ae23b72977208eeaad
Kurt Godden <kurtgodden@gmail.com>
2014-12-18 19:33:25 -0500

    Added line comments and comments with code used to test.

--- cachematrix.R
+++ cachematrix.R
@@ -9,6 +9,8 @@
 ## been computed/saved) or else it retrieves
 ## a previously computed/saved inverse.
 
+## Comments at the bottom show the code used to test these functions.
+
 ## Function ‘makeCacheMatrix’ returns a list of 4 functions:
 ## ‘set’ saves the input matrix in static var ‘x’ and
 ##  initializes static var ‘inverse’ to NULL
@@ -17,15 +19,17 @@
 ## ‘getinverse’ returns the value of ‘inverse’
 
 makeCacheMatrix <- function(x = matrix()) {
-    inverse <- NULL
-    set <- function(y) {
+    inverse <- NULL #init the cached inverse
+    set <- function(y) { #define fn to set cached matrix x
         x <<- y
-        inverse <<- NULL
+        inverse <<- NULL #reset inverse for new matrix
     }
-    get <- function() x
-    setinverse <- function(i) inverse <<- i
-    getinverse <- function() inverse
-    list(set = set, get = get,
+    get <- function() x #define fn to return cached matrix x
+    setinverse <- function(i) inverse <<- i #define fn to cache inverse
+    getinverse <- function() inverse #define fn to get cached inverse
+    #now return list of these 4 fns
+    list(set = set, 
+         get = get,
          setinverse = setinverse,
          getinverse = getinverse)
 }
@@ -38,15 +42,31 @@ makeCacheMatrix <- function(x = matrix()) {
 ## functions created by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-    ## Return a matrix that is the inverse of 'x'
-
-    inverse <- x$getinverse()
+    inverse <- x$getinverse() #retrieve from cache
     if(!is.null(inverse)) {
         message("getting cached inverse")
-        return(inverse)
+        return(inverse) #return it without recomputing
     }
-    data <- x$get()
-    inverse <- solve(data, ...)
-    x$setinverse(inverse)
-    inverse
+    data <- x$get() #get cached matrix
+    inverse <- solve(data, ...) #compute its inverse
+    x$setinverse(inverse) #save the inverse in cache
+    inverse #and return that now-saved inverse
 }
+
+# The following tests were used on these functions:
+# hilbert <- function(n) { i <- 1:n; 1 / outer(i - 1, i, "+") }
+# h8 <- hilbert(8)
+# h8_inverse <- solve(h8)
+# testCache8 <- makeCacheMatrix(h8)
+# testCache8$get()
+# testCache8$getinverse()
+# cacheSolve(testCache8)
+# testCache8$get() == h8
+# testCache8$getinverse() == h8_inverse
+# cacheSolve(testCache8)
+# h9 <- hilbert(9)
+# testCache9 <- makeCacheMatrix(h9)
+# testCache9$get()
+# testCache9$getinverse()
+# cacheSolve(testCache9)
+# cacheSolve(testCache9)
